2f1715fcc67677bbb53781c9de570417757ac10e
Prem Nath <prem.nath2700@gmail.com>
2025-07-02 23:22:38 +0530

    Adding First Test Case method - greetingsFromJenkinTest()

--- src/test/java/com/jenkins/jenkinsdemo/JenkinsdemoApplicationTests.java
+++ src/test/java/com/jenkins/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -1,13 +1,35 @@
 package com.jenkins.jenkinsdemo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JenkinsdemoApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void greetingsFromJenkinTest() throws Exception {
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/jenkinsdemo/greetings").accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(request).andReturn();
+		String contentAsString = result.getResponse().getContentAsString();
+		
+		assertEquals("Hello from Jenkins!", contentAsString);
+		assertNotEquals("Hello from Jenkins!!!", contentAsString);
+		
 	}
 
 }
